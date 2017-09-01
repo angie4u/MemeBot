93974172b9c6fe0d9f83b8fc1da17b88c6500f54
Eunji <eunk@microsoft.com>
2017-09-01 09:35:31 +0900

    update image upload by url & parse return data

--- Service/assets/js/connectApi.js
+++ Service/assets/js/connectApi.js
@@ -27,6 +27,16 @@ $(document).ready(function () {
             }
         });
     });
+
+    
+
+});
+
+$(document).ready(function () {
+
+    $('#processImageButton').click(function (){
+            processImage();
+    });
 });
 
 $(document).ready(function () {
@@ -170,3 +180,62 @@ function parseErrorMessage(data){
     var responseValue = JSON.parse(object.responseText);
     var errorCode = responseValue.Code;
 }
+
+function processImage() {
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    var subscriptionKey = "2f2fd5e9edbe4c4db60c2c87c9304bf1";
+
+    // Replace or verify the region.
+    //
+    // You must use the same region in your REST API call as you used to obtain your subscription keys.
+    // For example, if you obtained your subscription keys from the westus region, replace
+    // "westcentralus" in the URI below with "westus".
+    //
+    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    // a free trial subscription key, you should not need to change this region.
+    var uriBase = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+    };
+
+    // Display the image.
+    var sourceImageUrl = document.getElementById("imgUrl").value;
+    document.querySelector("#sourceImage").src = sourceImageUrl;
+
+    // Perform the REST API call.
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+
+    .done(function(data) {
+        // Show formatted JSON on webpage.
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+    })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+        alert(errorString);
+    });
+};
